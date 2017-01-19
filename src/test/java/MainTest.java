6a29a73fb5e098c52e90a6c8cd3c1bf4dd14198d
Bobby Raju <bobby.raju@swisscom.com>
2017-01-19 11:08:18 +0000

    Added a failing test

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+
+/**
+ * Created by ape11 on 19/01/2017.
+ */
+public class MainTest {
+    @Test
+    public void initialTest() {
+        Assert.assertTrue( 1 == 2);
+    }
+}
